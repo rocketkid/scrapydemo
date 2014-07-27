050ab82dd651d99579a8edaba9ea97cb83fe5927
gauravk <gauravk@flipkart.com>
2014-07-28 01:24:26 +0530

    Adding mysql store pipeline to store dmoz items

--- scrapydemo/pipelines.py
+++ scrapydemo/pipelines.py
@@ -1,4 +1,25 @@
+# Pipelines
 
-class ScrapydemoPipeline(object):
+import MySQLdb
+
+class DmozItemStorePipeline(object):
+	ADD_ITEM_QUERY = ("INSERT INTO DMOZ_ITEMS "
+			"(name, url, description) "
+			"VALUES (%s, %s, %s)"
+			)
+
+	def __init__(self):
+		db_host = settings['MYSQL_HOST']
+		db_name = settings['MYSQL_DBNAME']
+		db_user = settings['MYSQL_USER']
+		db_password = settings['MYSQL_PASSWD']
+        
+        self.db = MySQLdb.connect( db_host, db_user, db_password, db_name)
+        self.dbc = self.db.cursor() 
+
     def process_item(self, item, spider):
-        return item
+    	add_item_data = (item['name'], item['url'], item['description'])
+    	self.dbc.execute(ADD_ITEM_QUERY, add_item_data)
+    	return item
+
+
--- scrapydemo/settings.py
+++ scrapydemo/settings.py
@@ -3,3 +3,11 @@ BOT_NAME = 'scrapydemo'
 SPIDER_MODULES = ['scrapydemo.spiders']
 NEWSPIDER_MODULE = 'scrapydemo.spiders'
 
+ITEM_PIPELINES = ['scrapydemo.pipelines.DmozItemStorePipeline']
+
+MYSQL_HOST = 'localhost'
+MYSQL_DBNAME = 'scrapydemo'
+MYSQL_USER = 'root'
+MYSQL_PASSWD = ''
+
+
