89fefe0a561df25614c831409ae66564f87a0f62
gauravk <gauravk@flipkart.com>
2014-07-28 10:32:09 +0530

    Adding pipeline to store data into mysql

--- scrapydemo/pipelines.py
+++ scrapydemo/pipelines.py
@@ -1,25 +1,36 @@
 # Pipelines
 
 import MySQLdb
+from scrapy.exceptions import DropItem
 
 class DmozItemStorePipeline(object):
-	ADD_ITEM_QUERY = ("INSERT INTO DMOZ_ITEMS "
-			"(name, url, description) "
-			"VALUES (%s, %s, %s)"
-			)
 
-	def __init__(self):
-		db_host = settings['MYSQL_HOST']
-		db_name = settings['MYSQL_DBNAME']
-		db_user = settings['MYSQL_USER']
-		db_password = settings['MYSQL_PASSWD']
-        
-        self.db = MySQLdb.connect( db_host, db_user, db_password, db_name)
-        self.dbc = self.db.cursor() 
+    def __init__(self, db_conn):
+    	self.db_conn = db_conn
+    	self.db_cursor = db_conn.cursor()
+
+    @classmethod
+    def from_settings(cls, settings):
+    	db_host = settings['MYSQL_HOST']
+    	db_name = settings['MYSQL_DBNAME']
+    	db_user = settings['MYSQL_USER']
+    	db_passwd = settings['MYSQL_PASSWD']
+
+        db_conn = MySQLdb.connect(db_host, db_user, db_passwd, db_name)
+        return cls(db_conn)
 
     def process_item(self, item, spider):
-    	add_item_data = (item['name'], item['url'], item['description'])
-    	self.dbc.execute(ADD_ITEM_QUERY, add_item_data)
-    	return item
+        name = ''.join(item['name'])
+        url = ''.join(item['url'])
+        description = ''.join(item['description'])
 
+    	try:
+            self.db_cursor.execute('''INSERT INTO DMOZ_ITEMS (name, url, description) VALUES (%s, %s, %s)''', (name, url, description))
+            self.db_conn.commit()
+            return item
+        except MySQLdb.Error, e:
+            print "Error %d: %s" % (e.args[0],e.args[1])
+            self.db_conn.rollback()
+            raise DropItem("Unable to insert item: %s" % item)
 
+       
--- scrapydemo/settings.py
+++ scrapydemo/settings.py
@@ -3,7 +3,10 @@ BOT_NAME = 'scrapydemo'
 SPIDER_MODULES = ['scrapydemo.spiders']
 NEWSPIDER_MODULE = 'scrapydemo.spiders'
 
-ITEM_PIPELINES = ['scrapydemo.pipelines.DmozItemStorePipeline']
+#ITEM_PIPELINES = ['scrapydemo.pipelines.DmozItemStorePipeline']
+ITEM_PIPELINES = {
+    'scrapydemo.pipelines.DmozItemStorePipeline': 1   
+}
 
 MYSQL_HOST = 'localhost'
 MYSQL_DBNAME = 'scrapydemo'
